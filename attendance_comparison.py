ce53a8bd57f6d30fe9f85cfd4ba7ee3fa54a1203
datacloudgui <guillermosanchezh@gmail.com>
2020-08-16 21:06:30 -0500

    End comparison, pending finish report and args

--- attendance_comparison.py
+++ attendance_comparison.py
@@ -1,4 +1,5 @@
 import openpyxl
+from openpyxl import Workbook
 
 def open_excel_sheet(filename, sheet_name):
     wb = openpyxl.load_workbook(filename)
@@ -29,7 +30,39 @@ def extract_list_by_index(sheet, row_start, column):
             students.append(sheet[column + str(row)].value)
     return students
 
+def attended_split(list_students, attendance_students):
+    #Use the oficial list to split attendance list in two list:
+    #attended in list (attended_students) and attended out of list (attended_not_in_list)
 
+    attended_students = []
+    attended_not_in_list = attendance_students
+
+    for student in list_students:
+        #Split the name in official list
+        words_name = student.lower().split(' ')
+        word_find_count = 0
+        for student_attended in attendance_students:
+            #Iterate over attendance list searching the splited name
+            for word in words_name:
+                #Search in the current attendance item for words in the name splited
+                if(student_attended.lower().find(word) != -1):
+                    word_find_count += 1
+                    if(word_find_count == 2):
+                        #Store attended official students if at least 2 words are founded
+                        # replace 2 with len(words_name) to verify all the words in the official list
+                        # at the end of the process attended_not_in_list will contain the remained names 
+                        #for people that attend the class but aren't in the official list.
+                        attended_students.append(student)
+                        attended_not_in_list.remove(student_attended)
+                        break
+    return attended_students, attended_not_in_list
+
+def extract_absence_students(list_students, attended_students):
+    absence_students = list_students
+
+    for item in attended_students:
+        absence_students.remove(item)
+    return absence_students
 
 if __name__ == "__main__":
     list_file='servos.xlsx'
@@ -48,9 +81,36 @@ if __name__ == "__main__":
     attendance_column_index = find_word_in_column(attendance_sheet,1,'servos')
     attendance_students = extract_list_by_index(attendance_sheet, 2, attendance_column_index[0:-1])
 
-    print('Lista de estudiantes')
-    for item in attendance_students:
+    attended_students, attended_not_in_list = attended_split(list_students, attendance_students)
+    absence_students = extract_absence_students(list_students, attended_students)
+    #print('Lista de estudiantes')
+    #for item in attendance_students:
+    #    print(item)
+    #print('Lista de estudiantes')
+    #for item in list_students:
+    #    print(item)
+
+    print('Asistentes en lista:')
+    for item in attended_students:
         print(item)
-    print('Lista de estudiantes')
-    for item in list_students:
+    print('----------------------------------')
+
+    print('Asistentes NO en lista')
+    for item in attended_not_in_list:        
         print(item)
+
+    print('----------------------------------')
+
+    print('Ausentes')
+    for item in absence_students:        
+        print(item)
+
+    book = Workbook()
+    sheet = book.active
+    sheet.title = 'servos'
+    column = 'A'
+    sheet['A1']='Asistentes en lista'
+    for row in range (0, len(attended_students)):
+        sheet[column + str(row+2)] = attended_students[row]
+
+    book.save("report.xlsx")
